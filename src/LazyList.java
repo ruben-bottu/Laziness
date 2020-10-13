76441a4f2954a886df0ffd8c69358e5bb308f439
Ruben Bottu <ruben.bottu@student.ucll.be>
2020-10-13 22:05:06 +0200

    Renamed EndLazyList -> EndNode, NormalLazyList -> NormalNode NormalNode and EndNode refactored to be Static Nested Classes Added two factory methods: one that accepts an Iterable, one that accepts varargs

--- src/LazyList.java
+++ src/LazyList.java
@@ -1,4 +1,6 @@
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
 import java.util.function.Function;
 import java.util.function.Predicate;
@@ -7,9 +9,25 @@ public abstract class LazyList<E> {
     public final Lazy<E> value;
     public final Lazy<LazyList<E>> tail;
 
-    public LazyList(Lazy<E> value, Lazy<LazyList<E>> tail) {
+    private LazyList(Lazy<E> value, Lazy<LazyList<E>> tail) {
         this.value = value;
         this.tail = tail;
+    }
+
+    private static <E> LazyList<E> ofHelper(Iterator<? extends E> iterator) {
+        if (iterator.hasNext()) {
+            return NormalNode.of(Lazy.of(iterator::next), Lazy.of(() -> ofHelper(iterator)));
+        }
+        return EndNode.empty();
+    }
+
+    public static <E> LazyList<E> of(Iterable<? extends E> elements) {
+        return ofHelper(elements.iterator());
+    }
+
+    @SafeVarargs
+    public static <E> LazyList<E> of(E... elements) {
+        return LazyList.of(Arrays.asList(elements));
     }
 
     public abstract <R> LazyList<R> map(Function<E, R> transform);
@@ -17,12 +35,63 @@ public abstract class LazyList<E> {
     public abstract LazyList<E> filter(Predicate<E> predicate);
 
     private List<E> toListHelper(List<E> result) {
-        if (this instanceof EndLazyList) return result;
+        if (this instanceof EndNode) return result;
         result.add(value.value());
         return tail.value().toListHelper(result);
     }
 
     public List<E> toList() {
         return toListHelper(new ArrayList<>());
+    }
+
+
+
+    private static class NormalNode<E> extends LazyList<E> {
+
+        private NormalNode(Lazy<E> value, Lazy<LazyList<E>> tail) {
+            super(value, tail);
+        }
+
+        public static <E> LazyList<E> of(Lazy<E> value, Lazy<LazyList<E>> tail) {
+            return new NormalNode<>(value, tail);
+        }
+
+        @Override
+        public <R> LazyList<R> map(Function<E, R> transform) {
+            return NormalNode.of(
+                    Lazy.of(() -> transform.apply(value.value())),
+                    Lazy.of(() -> tail.value().map(transform))
+            );
+        }
+
+        @Override
+        public LazyList<E> filter(Predicate<E> predicate) {
+            return predicate.test(value.value()) ?
+                    NormalNode.of(value, Lazy.of(() -> tail.value().filter(predicate))) :
+                    tail.value().filter(predicate);
+        }
+    }
+
+
+
+    private static class EndNode<E> extends LazyList<E> {
+
+        private EndNode(Lazy<E> value, Lazy<LazyList<E>> tail) {
+            super(value, tail);
+        }
+
+        public static <E> LazyList<E> empty() {
+            return new EndNode<>(null, null);
+        }
+
+        @Override
+        public <R> LazyList<R> map(Function<E, R> transform) {
+            return EndNode.empty();
+        }
+
+        @Override
+        public LazyList<E> filter(Predicate<E> predicate) {
+            return this;
+        }
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
         Lazy<Integer> x = Lazy.of( () -> 1 + 2 );
@@ -15,9 +17,22 @@ public class Main {
         //LazyList<Integer> list5 = node1.map(num -> num + 10);
         /*LazyList<Integer> list5 = node1.filter(num -> num > 8);
         System.out.println(list5.toList());*/
-        LazyList<Integer> lazyList3 = NormalLazyList.of(Lazy.of(() -> 1), Lazy.of(EndLazyList::empty));
-        LazyList<Integer> lazyList2 = NormalLazyList.of(Lazy.of(() -> -9), Lazy.of(() -> lazyList3));
-        LazyList<Integer> list = NormalLazyList.of(Lazy.of(() -> 50), Lazy.of(() -> lazyList2));
-        System.out.println(list.filter(e -> e < 5).map(e -> e + 10).toList());
+        /*LazyList<Integer> lazyList3 = LazyList.normalOf(Lazy.of(() -> 1), Lazy.of(LazyList::endOf));
+        LazyList<Integer> lazyList2 = LazyList.normalOf(Lazy.of(() -> -9), Lazy.of(() -> lazyList3));
+        LazyList<Integer> list = LazyList.normalOf(Lazy.of(() -> 50), Lazy.of(() -> lazyList2));*/
+        //System.out.println(list.filter(e -> e < 5).map(e -> e + 10).toList());
+        ArrayList<String> cars = new ArrayList<>();
+        cars.add("Volvo");
+        cars.add("BMW");
+        cars.add("Ford");
+        cars.add("Mazda");
+
+        LazyList<String> lazyList = LazyList.of(cars);
+        LazyList<String> lazyList1 = LazyList.of("foo", "bar", "ye");
+        LazyList<Integer> lazyList4 = LazyList.of(50, -9, 1);
+        //System.out.println(lazyList1.toList());
+        LazyList<Animal> animalLazyList = LazyList.of(new Dog(12, "jef"), new Dog(5, "johanna"));
+        System.out.println(animalLazyList.map(Animal::getAge).toList());
+        System.out.println(lazyList4.value.value() + " ;;; " + lazyList4.tail.value().value.value());
     }
 }
