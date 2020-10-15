d168a83f3544f5587872d5c112ecc20ab9d7e60d
Ruben Bottu <ruben.bottu@student.ucll.be>
2020-10-15 23:46:43 +0200

    Added Iterator class for LazyList

---
+++ src/LazyListIterator.java
@@ -0,0 +1,25 @@
+import java.util.Iterator;
+
+public class LazyListIterator<E> implements Iterator<E> {
+    private LazyList<E> currentNode;
+
+    private LazyListIterator(LazyList<E> elements) {
+        currentNode = elements;
+    }
+
+    public static <E> LazyListIterator<E> of(LazyList<E> elements) {
+        return new LazyListIterator<>(elements);
+    }
+
+    @Override
+    public boolean hasNext() {
+        return currentNode.tail.value().any();
+    }
+
+    @Override
+    public E next() {
+        E nextValue = currentNode.value.value();
+        currentNode = currentNode.tail.value();
+        return nextValue;
+    }
+}
